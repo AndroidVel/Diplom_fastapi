50da7ff48a8f916309f66ba7b49c43b18687e61b
Андрей <andreyv006@mail.ru>
2024-11-21 21:55:42 +0100

    product model

---
+++ models/product.py
@@ -0,0 +1,13 @@
+from backend.db import Base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from models import *
+
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    price = Column(Float, nullable=False)
+    weight = Column(Float)
+    image_url = Column(String, nullable=False)
